84c3cdb5a07f2bc7ca65c885fc7f7c1fa80740ff
Michael <michael.heinitz@adidas.com>
2018-04-16 10:39:27 +0200

    Button 'Add Server' added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } , 2000);
+  }
 
   ngOnInit() {
   }
+
+
 
 }
